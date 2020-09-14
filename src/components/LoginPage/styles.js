45fc21789b6c79c656391e9b9fe962080467f63d
klepa <pedro.klepa@gmail.com>
2020-09-14 13:01:17 -0300

    Remove deprecated hidden class

--- src/components/LoginPage/styles.js
+++ src/components/LoginPage/styles.js
@@ -12,10 +12,6 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
-  &.hidden {
-    display: none;
-  }
 `;
 
 export const Form = styled.div`
