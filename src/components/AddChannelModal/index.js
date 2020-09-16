529dd99a8f1c2e80fdd9aac130ab063cdbbfe456
klepa <pedro.klepa@gmail.com>
2020-09-15 22:04:33 -0300

    Add function to close the modal

--- src/components/AddChannelModal/index.js
+++ src/components/AddChannelModal/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import {
   Container,
   Form,
@@ -12,43 +12,66 @@ import {
   Button,
 } from "./styles";
 
-const AddChannelModal = ({ isVisible, setIsVisible }) => (
-  <AnimatePresence>
-    {isVisible && (
-      <Container
-        onClick={() => setIsVisible(false)}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
-        <Form
-          onClick={(e) => e.stopPropagation()}
-          initial={{ y: 100 }}
-          animate={{ y: 0 }}
-          exit={{ y: 50 }}
+function AddChannelModal({ isVisible, setIsVisible }) {
+  const [channelName, setChannelName] = useState("");
+  const [channelDescription, setChannelDescription] = useState("");
+
+  function closeModal() {
+    setIsVisible(false);
+    setChannelName("");
+    setChannelDescription("");
+  }
+
+  return (
+    <AnimatePresence>
+      {isVisible && (
+        <Container
+          onClick={closeModal}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
         >
-          <Header>
-            <h1>Create text channel</h1>
-          </Header>
+          <Form
+            onClick={(e) => e.stopPropagation()}
+            initial={{ y: 100 }}
+            animate={{ y: 0 }}
+            exit={{ y: 50 }}
+          >
+            <Header>
+              <h1>Create text channel</h1>
+            </Header>
 
-          <InputWrapper>
-            <Label>Channel name</Label>
-            <Input type="text" maxLength="20" />
-          </InputWrapper>
+            <InputWrapper>
+              <Label>Channel name</Label>
+              <Input
+                type="text"
+                maxLength="20"
+                value={channelName}
+                onChange={(e) => setChannelName(e.target.value)}
+              />
+            </InputWrapper>
 
-          <InputWrapper>
-            <Label>Channel description</Label>
-            <Input type="text" maxLength="80" />
-          </InputWrapper>
+            <InputWrapper>
+              <Label>Channel description</Label>
+              <Input
+                type="text"
+                maxLength="80"
+                value={channelDescription}
+                onChange={(e) => setChannelDescription(e.target.value)}
+              />
+            </InputWrapper>
 
-          <Footer>
-            <Button isCancel>Cancel</Button>
-            <Button>Create channel</Button>
-          </Footer>
-        </Form>
-      </Container>
-    )}
-  </AnimatePresence>
-);
+            <Footer>
+              <Button onClick={closeModal} isCancel>
+                Cancel
+              </Button>
+              <Button>Create channel</Button>
+            </Footer>
+          </Form>
+        </Container>
+      )}
+    </AnimatePresence>
+  );
+}
 
 export default AddChannelModal;
