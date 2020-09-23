83fa2f52842a18006a2e626611a17afa00b9362e
klepa <pedro.klepa@gmail.com>
2020-09-23 16:28:30 -0300

    Updates

---
+++ precache-manifest.11ba27568e22bd0a2620cdb1b8a8f324.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51386cad62085f643a60a40c65e3d9c9",
+    "url": "/discord-clone/index.html"
+  },
+  {
+    "revision": "fbd4d8354e7ce75c8310",
+    "url": "/discord-clone/static/js/2.55e47be2.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/discord-clone/static/js/2.55e47be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b7043b7153b98fcefe",
+    "url": "/discord-clone/static/js/main.8f722935.chunk.js"
+  },
+  {
+    "revision": "7513c738c2e2ffe4d32a",
+    "url": "/discord-clone/static/js/runtime-main.f533448a.js"
+  },
+  {
+    "revision": "09f27433693de215421c5ce916309021",
+    "url": "/discord-clone/static/media/discord.09f27433.svg"
+  },
+  {
+    "revision": "fd91131ea693096d6be5e8aa99d18f9e",
+    "url": "/discord-clone/static/media/discordBackground.fd91131e.jpg"
+  },
+  {
+    "revision": "12426016f217b0a158022b8b99ecd492",
+    "url": "/discord-clone/static/media/red-bar.12426016.svg"
+  }
+]);
